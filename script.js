56c3744a16a2f8e598d0e3301d05b3544297e9f1
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-09-25 17:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+ function functionName() {
+      
+      alert(arguments.callee.name);
+    }
+
+    
+    functionName();  
+
+    function AccioJob() {
+      alert(arguments.callee.name);
+    }
